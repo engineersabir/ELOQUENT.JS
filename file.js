c97a465a7abfa6b5eafb545859d622cce2a62460
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-12 20:22:23 +0500

    Update index.html and style.css

---
+++ file.js
@@ -0,0 +1,16 @@
+// Automatic type Conversion
+ 
+console.log(8*null)
+// -> 0
+console.log("5"-1);
+// ->4
+console.log("5"+1);
+// -> 51
+console.log("five"*2)
+//NaN
+console.log(false==0)
+//True
+console.log(null==undefined);
+//True
+console.log(null==0);
+//False
