68f898c47292e61c7b6e98de6611f7fdbacda462
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-14 19:25:52 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -1,16 +1,20 @@
-// Automatic type Conversion
+// // Automatic type Conversion
  
-console.log(8*null)
-// -> 0
-console.log("5"-1);
-// ->4
-console.log("5"+1);
-// -> 51
-console.log("five"*2)
-//NaN
-console.log(false==0)
-//True
-console.log(null==undefined);
-//True
-console.log(null==0);
-//False
+// console.log(8*null)
+// // -> 0
+// console.log("5"-1);
+// // ->4
+// console.log("5"+1);
+// // -> 51
+// console.log("five"*2)
+// //NaN
+// console.log(false==0)
+// //True
+// console.log(null==undefined);
+// //True
+// console.log(null==0);
+// //False
+console.log(null||"user");
+// -> user 
+console.log("agnes"||"user");
+// -> Agnes
