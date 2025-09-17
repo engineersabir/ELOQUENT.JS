43d63f9ff48d1fa4bfd7c0134bb8e45995669075
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-17 11:55:20 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -20,36 +20,36 @@
 // -> Agnes
 // let ten=10;
 // console.log(ten*ten)
-// -> 100
-let mood="light";
-console.log(mood)
-// -> light
-mood="dark";
-console.log(mood);
-// -> dark
-let luigisDebt=140;
-luigisDebt=luigisDebt-35;
-// console.log(luigisDebt);
-// -> 105
-let one=1, two=2;
-// console.log(one+two);
-// -> 3
-var name="Ayda";
-const greeting="hello ";
-// console.log(greeting+name)
-// -> hello Ayda
+// // -> 100
+// let mood="light";
+// console.log(mood)
+// // -> light
+// mood="dark";
+// console.log(mood);
+// // -> dark
+// let luigisDebt=140;
+// luigisDebt=luigisDebt-35;
+// // console.log(luigisDebt);
+// // -> 105
+// let one=1, two=2;
+// // console.log(one+two);
+// // -> 3
+// var name="Ayda";
+// const greeting="hello ";
+// // console.log(greeting+name)
+// // -> hello Ayda
 
-let theNumber=Number(prompt("pick a number"));
-if (!isNaN(theNumber)){
-    console.log("your number is the square root of "+theNumber*theNumber);
+// let theNumber=Number(prompt("pick a number"));
+// if (!isNaN(theNumber)){
+//     console.log("your number is the square root of "+theNumber*theNumber);
 
-}
-else{
-    console.log("Hey . Why didn't you give me a number?")
-}
-;
+// }
+// else{
+//     console.log("Hey . Why didn't you give me a number?")
+// }
+// ;
 
-if (1+1==2)console.log("Its True")
+// if (1+1==2)console.log("Its True")
     // -> its true
 let num= Number(prompt("Pick a number"));
 if (num<10){
