22596f8b2b7fe75c2e0edb95673447596557db4a
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-17 21:08:48 +0500

    code practice is done for today

--- string.js
+++ string.js
@@ -8,12 +8,24 @@ var a="99";
 var num=parseInt(a);
 console.log(num);
 let b="100";
-let bun=Number.isInteger(b);
-console.log(bun);
+// let bun=Number.isInteger(b);
+// console.log(bun);
 let c=5.6789;
-let can=Number.toFixed(3);
-console.log(can);
-console.log(can);
-console.log(can);
-console.log(can);
-console.log(can);
+// let can=Number.toFixed(3);
+// console.log(can);
+
+// LITERALS IN JAVA SCRIPT
+// Object {}
+// Boolean true, false
+// string '', ""
+//Template Literal ``
+
+const another=`This is my first message`;
+console.log(another);
+
+const message='John';
+const another1=`Hi ${message},
+Thank You for joining my mailing list .
+Regards ,
+Sabir`;
+console.log(another1);
