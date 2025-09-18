8fe99cc16228b0a2c75148018ac3165c62b39302
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-18 12:10:06 +0500

    Update index.html and style.css

--- loops.js
+++ loops.js
@@ -1,29 +1,45 @@
-let number =0;
-while(number<=12){
-    console.log(number);
-    number=number+2;
-}
- let result =1;
- let counter =0;
- while (counter<10){
-    result =result*2;
-    counter=counter+1;
- }
- console.log(result);
- // -> 1024
- let yourName;
- do{
-    yourName=prompt(" who are you?");
- }while(!yourName);
- console.log(yourName);
-// console.clear()
+// let number =0;
+// while(number<=12){
+//     console.log(number);
+//     number=number+2;
+// }
+//  let result =1;
+//  let counter =0;
+//  while (counter<10){
+//     result =result*2;
+//     counter=counter+1;
+//  }
+//  console.log(result);
+//  // -> 1024
+//  let yourName;
+//  do{
+//     yourName=prompt(" who are you?");
+//  }while(!yourName);
+//  console.log(yourName);
+// // console.clear()
 
-const string="The revolution willnot be televised";
-console.log(string)
-const one="hello, "
-const two="how are you ?"
-const joined=`${one}${two}`
-console.log(joined);
-const name =" chris";
-const greeting=`Hello, ${name}`;
-console.log(greeting);
+// const string="The revolution willnot be televised";
+// console.log(string)
+// const one="hello, "
+// const two="how are you ?"
+// const joined=`${one}${two}`
+// console.log(joined);
+// const name =" chris";
+// const greeting=`Hello, ${name}`;
+// console.log(greeting);
+
+if (false !=true){
+   console.log("That makes sense,");
+   if(1<2)
+{
+   console.log("No surprise there.")
+}
+};
+for(let number=0; number<=12;number=number+3){
+   console.log(number);
+}
+let result=1;
+for(let counter=0;counter<10;counter=counter+1){
+   result=result*2;
+}
+console.log(result);
